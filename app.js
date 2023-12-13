81fa94a63311fbcee4c459bd719d0701afce4fce
Hols <samettcaner@gmail.com>
2023-12-13 16:05:50 +0300

    ejs update

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
+//Template Engine
+app.set('view engine', 'ejs');
+
+//Public file saved. || MIDDLEWARE
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog Title', description: 'Blog description' };
-  res.send(blog);
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 const port = 3000;
